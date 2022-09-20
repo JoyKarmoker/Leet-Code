79041bd5eabdbc614886b9af8419e952e92e8e8f
JoyKarmoker <Joykarmoker353@gmail.com>
2022-09-20 06:41:15 +0600

    1991. Find the Middle Index in Array

---
+++ 1991_Find the Middle Index in Array2.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int pivotIndex(vector<int>& nums) {
+        
+        int sum = accumulate(nums.begin(), nums.end(), 0);
+        int length = nums.size();
+        int leftSum = 0;
+        int rightSum = 0;
+        
+        if(sum - nums[0] == 0)
+        {
+            return 0;
+        }      
+        
+        for(int i=1; i<length; i++)
+        {
+            leftSum = leftSum + nums[i-1];
+            rightSum = sum - leftSum - nums[i];
+            
+            if(leftSum == rightSum)
+            {
+                return i;
+            }
+        }
+        
+        return -1;
+        
+    }
+};
